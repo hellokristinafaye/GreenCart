5b5da2cbd993d5544cb1e3f446f71a02d632e816
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-14 15:23:41 -0700

    the routes/route tags aren't importing properly, and nothing is displaying within them. Outside the Routes tag stuff does show up, so I think it's a Routes or react-router-dom issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom' 
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <div>
+        hello
+        <Routes>
+          hello
+          <Route />
+        </Routes>
+      </div>
     </div>
   )
 }
