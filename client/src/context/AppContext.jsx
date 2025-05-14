5ee32e0b79a2ef005653c2f734dbb4420cc07b43
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-13 20:35:06 -0700

    FOUND IT. I was mising a line and a variable from the AppContext.jsx file.  Amaze.

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
