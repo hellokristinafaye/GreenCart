00a266b7238de252d1f6c87114e4a1f13a224a1a
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-14 15:05:19 -0700

    banner started. file structure and basic connections. Img. Now adding styling via Tailwind

---
+++ client/src/components/MainBanner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const MainBanner = () => {
+  return (
+    <div>
+        <img src={assets.main_banner_bg} alt="banner" className="" />      
+    </div>
+  )
+}
+
+export default MainBanner
