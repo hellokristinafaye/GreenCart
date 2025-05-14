34456c59c4bcf0f2d2ffd27a85f9a2db0032d32c
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-14 16:13:59 -0700

    Link to products added

--- client/src/components/MainBanner.jsx
+++ client/src/components/MainBanner.jsx
@@ -11,7 +11,12 @@ const MainBanner = () => {
             <h1 className="">Freshness You Can Trust, Savings You will Love! </h1>
           </div>  
           <div className="">
-              <Link className="group flex items-center gap-2 px-7 md:px-9 py-3 bg-primary hover:bg-primary-dull transition rounded text-white cursor-pointer">
+              <Link to={"/products"} className="group flex items-center gap-2 px-7 md:px-9 py-3 bg-primary hover:bg-primary-dull transition rounded text-white cursor-pointer">
+                  Shop now
+                  <img className="md:hidden transition group-focus:translate-x-1" src={assets.white_arrow_icon} alt="arrow"  />
+              </Link>
+              
+              <Link to={"/products"} className="group flex items-center gap-2 px-7 md:px-9 py-3 bg-primary hover:bg-primary-dull transition rounded text-white cursor-pointer">
                   Shop now
                   <img className="md:hidden transition group-focus:translate-x-1" src={assets.white_arrow_icon} alt="arrow"  />
               </Link>
