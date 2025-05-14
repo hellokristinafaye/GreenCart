c94ae8aaaa2d23bc6e2f2cc5fec24851fa079258
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-14 15:13:06 -0700

    correcting a couple missed bits and deleting the last mounting we did b/c lol he's covering that in the next step

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import MainBanner from './components/MainBanner'
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <MainBanner />
     </div>
   )
 }
--- client/src/components/MainBanner.jsx
+++ client/src/components/MainBanner.jsx
@@ -3,9 +3,9 @@ import { assets } from '../assets/assets'
 
 const MainBanner = () => {
   return (
-    <div>
+    <div className='relative'>
         <img src={assets.main_banner_bg} alt="banner" className="w-full hidden md:block" />      
-        <img src={assets.main_banner_bg_sm} alt="banner" className="w-full hidden md:hidden" />      
+        <img src={assets.main_banner_bg_sm} alt="banner" className="w-full md:hidden" />      
     </div>
   )
 }
