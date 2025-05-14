647cb95f7d6554c4e7f94eecfa912519ba0e6b0d
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-14 15:26:30 -0700

    YES Route/Routes successfully auto-imported. Start with the angle bracket, that triggers the menu

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import { Route, Routes } from 'react-router-dom' 
+// import { Route, Routes } from 'react-router-dom' 
 import Home from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <div>
-        hello
+        
         <Routes>
-          hello
           <Route />
         </Routes>
       </div>
