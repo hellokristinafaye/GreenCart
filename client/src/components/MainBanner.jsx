b8fb6ab70a1e849e02a2f9cda3c1a0a88933057e
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-14 16:12:48 -0700

    OK it's there just currently hidden per CSS

--- client/src/components/MainBanner.jsx
+++ client/src/components/MainBanner.jsx
@@ -13,7 +13,7 @@ const MainBanner = () => {
           <div className="">
               <Link className="group flex items-center gap-2 px-7 md:px-9 py-3 bg-primary hover:bg-primary-dull transition rounded text-white cursor-pointer">
                   Shop now
-                  <img src={assets.white_arrow_icon} alt="arrow" className="md:hidden transition group-focus:translate-x-1" />
+                  <img className="md:hidden transition group-focus:translate-x-1" src={assets.white_arrow_icon} alt="arrow"  />
               </Link>
           </div>
     </div>
