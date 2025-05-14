efecfa87d1920f7c4b5c07e0d7161b8c5b70f10f
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-14 14:15:45 -0700

    mobile menu open/close toggle worksgit add .

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -46,7 +46,8 @@ const Navbar = () => {
             </button>
 
             {/* Mobile Menu */}
-            <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
+            { open && (
+                <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
                 <NavLink to="/" onClick={() => setOpen(false)}>Home</NavLink>
                 <NavLink to="/products" onClick={() => setOpen(false)}>All Product</NavLink>
                 {user &&
@@ -68,6 +69,7 @@ const Navbar = () => {
                 )}
                 
             </div>
+            )}
 
         </nav>
   )
