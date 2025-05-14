80abfdef68622b2aa756c4572bac2e749f1c3a79
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-14 15:09:19 -0700

    since we like to see the incrememental updates we CAN just add the component to App.jsx first thing. Then we can see the updates as we code. May someday be in Main.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import MainBanner from './components/MainBanner'
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <MainBanner />
     </div>
   )
 }
--- client/src/components/MainBanner.jsx
+++ client/src/components/MainBanner.jsx
@@ -4,7 +4,8 @@ import { assets } from '../assets/assets'
 const MainBanner = () => {
   return (
     <div>
-        <img src={assets.main_banner_bg} alt="banner" className="" />      
+        <img src={assets.main_banner_bg} alt="banner" className="w-full hidden md:block" />      
+        <img src={assets.main_banner_bg_sm} alt="banner" className="w-full hidden md:hidden" />      
     </div>
   )
 }
