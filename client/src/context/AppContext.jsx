05fbfc7a7c104b18f8b8f1255727932b07d3777b
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-15 16:59:21 -0700

    debugged. Needed to export cartItems from AppContext

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -63,7 +63,7 @@ export const AppContextProvider = ({ children }) => {
     },[])
 
     const value = {
-        navigate, user, setUser, setIsSeller, isSeller, showUserLogin, setShowUserLogin, products, currency, addToCart, updateCartItems, removeFromCart
+        navigate, user, setUser, setIsSeller, isSeller, showUserLogin, setShowUserLogin, products, currency, addToCart, updateCartItems, removeFromCart, cartItems
     };
     
     return <AppContext.Provider value={value}>
