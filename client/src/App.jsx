8cbe7ff68fba11e9928ed4bba964ad837f33d4ef
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-15 15:36:05 -0700

    categories section start

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-// import { Route, Routes } from 'react-router-dom' 
 import Home from './pages/Home'
 import { Route, Routes, useLocation } from 'react-router-dom'
 
