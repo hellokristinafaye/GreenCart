a77ee0e58e5c0ef7e48b16887849b08c8c5ec51d
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-15 16:11:26 -0700

    products state added

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -9,6 +9,7 @@ export const AppContextProvider = ({ children }) => {
     const [user, setUser] = useState(null)
     const [isSeller, setIsSeller] = useState(false)
     const [showUserLogin, setShowUserLogin] = useState(false)
+    const [products, setProducts] = useState([])
 
     const value = {
         navigate, user, setUser, setIsSeller, isSeller, showUserLogin, setShowUserLogin
