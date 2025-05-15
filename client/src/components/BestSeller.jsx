d04b07707590d6ce8686d8f429eb45686366b009
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-15 16:00:28 -0700

    Best Sellers section started

---
+++ client/src/components/BestSeller.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const BestSeller = () => {
+  return (
+      <div className="mt-16">
+          <p className="">Best Sellers</p>
+          <div className="">
+              
+          </div>
+    </div>
+  )
+}
+
+export default BestSeller
