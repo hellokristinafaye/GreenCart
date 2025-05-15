0e175569fd92bb2f359e621dd352898c632d46e6
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-15 15:40:41 -0700

    P tag styled

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -3,8 +3,8 @@ import { assets } from '../assets/assets'
 
 const Categories = () => {
   return (
-    <div>
-          <p className="">Categories</p>
+    <div className="mt-16">
+          <p className="text-2xl md:text-3xl font-medium">Categories</p>
           <div className="">
               <div className="">
                   <img src={assets.box_icon} alt="" className="" />
