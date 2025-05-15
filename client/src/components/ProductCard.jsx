0e4f46c160866ff2da29af796645eb918b69ef53
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-15 16:54:01 -0700

    essentials imported from AppContext to ProductCard

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -5,7 +5,7 @@ import { useAppContext } from "../context/AppContext";
 
 const ProductCard = ({product}) => {
     const [count, setCount] = React.useState(0);
-    const { currency } = useAppContext();P
+    const { currency, addToCart, removeFromCart, cartItems, navigate } = useAppContext();
 
     return (
         <div className="border border-gray-500/20 rounded-md md:px-4 px-3 py-2 bg-white min-w-56 max-w-56 w-full">
