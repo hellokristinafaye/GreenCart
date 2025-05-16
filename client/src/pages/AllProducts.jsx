0335f8e88c885b6e557ff3f45893773d9052910f
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-16 12:06:43 -0700

    search and filter logic added

--- client/src/pages/AllProducts.jsx
+++ client/src/pages/AllProducts.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useAppContext } from '../context/AppContext'
 
 const AllProducts = () => {
@@ -6,7 +6,15 @@ const AllProducts = () => {
     const { products, searchQuery } = useAppContext();
     const [filteredProducts, setFilteredProducts] = useState([]);
 
-    
+    useEffect(() => {
+        if (searchQuery.length > 0) {
+            setFilteredProducts(products.filter(
+                product => product.name.toLowerCase().includes(searchQuery.toLowerCase())
+            ))
+        } else {
+            setFilteredProducts(products);
+        }
+    }, [products, searchQuery])
 
   return (
     <div className="mt-16 flex flex-col">
