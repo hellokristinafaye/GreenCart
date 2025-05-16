13b54bb0040ae2b86a3759b8f7ec109d60f7e1d6
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-16 12:47:23 -0700

    styling added to category

--- client/src/pages/ProductCategory.jsx
+++ client/src/pages/ProductCategory.jsx
@@ -14,7 +14,12 @@ const ProductCategory = () => {
 
   return (
     <div>
-          
+          {searchCategory && (
+              <div className="flex flex-col items-end w-max">
+                  <p className="text-2xl font-medium">{searchCategory.text.toUpperCase()}</p>
+                  <div className="w-16 h-0.5 bg-primary rounded-full"></div>
+              </div>
+        )}          
     </div>
   )
 }
