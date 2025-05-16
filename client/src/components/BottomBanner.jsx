aeb04d7d4a2c48d552ef1d60b0650220ec9cb12f
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-15 18:04:57 -0700

    bottom banner content added/styled

--- client/src/components/BottomBanner.jsx
+++ client/src/components/BottomBanner.jsx
@@ -5,7 +5,12 @@ const BottomBanner = () => {
   return (
     <div className="relative mt-24">
         <img src={assets.bottom_banner_image} alt="banner" className="w-full hidden md:block" />          
-        <img src={assets.bottom_banner_image_sm} alt="banner" className="w-full md:hidden" />          
+          <img src={assets.bottom_banner_image_sm} alt="banner" className="w-full md:hidden" />    
+          <div className="absolute inset-0 flex flex-col items-center md:items-end md:justify-center pt-16 md:pt-0 md:pr-24">
+              <div className="">
+                <h1 className="text-2xl md:text-3xl font-semibold text-primary mb-6">Why We Are the Best?</h1>
+              </div>  
+        </div>  
     </div>
   )
 }
