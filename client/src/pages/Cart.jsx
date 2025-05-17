38c273b2e97be94c056c0592bff26af6141fd250
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-16 17:58:11 -0700

    continue shopping button onclick added

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -71,7 +71,10 @@ const Cart = () => {
                     </div>)
                 )}
 
-                <button className="group cursor-pointer flex items-center mt-8 gap-2 text-indigo-500 font-medium">
+                <button onClick={() => {
+                    navigate('/products');
+                    scrollTo(0, 0);
+                }} className="group cursor-pointer flex items-center mt-8 gap-2 text-indigo-500 font-medium">
                     <img src={assets.arrow_right_icon_colored} alt="arrow" className="group-hover:-translate-x-1 transition" />
                     Continue Shopping
                 </button>
