e86824beb3402d1a516647cc3ace8783a04ec97d
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-18 14:31:03 -0700

    input form stylying added

--- client/src/pages/AddAddress.jsx
+++ client/src/pages/AddAddress.jsx
@@ -4,13 +4,13 @@ import { assets } from '../assets/assets'
 // Input Field Component
 const inputField = ({ type, placeholder, name, handleChange, address }) => (
     <input
+        className="w-full px-2 py-2.5 border border-gray-500/30 rounded outline-none text-gray-500 focus:border-primary transition"
         type={type} 
         placeholder={placeholder}
         onChange={handleChange}
         name={name}
         value={address[name]}
         required
-        className=""
     />
 
 )
