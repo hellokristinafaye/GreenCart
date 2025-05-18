552da4a077226c66e704699cee8e66ffc3c457ec
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-18 14:18:30 -0700

    Add Address component started

--- client/src/pages/AddAddress.jsx
+++ client/src/pages/AddAddress.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
+// Input Field Component
+const inputField = (() => (
+    <input type="text" className="" />
+))
+
 const AddAddress = () => {
   return (
-    <div>AddAddress</div>
+    <div className="mt-16 pb-16">
+        <p className="text-2xl md:text-3xl text-gray-500">Add Shipping <span className="font-semibold text-primary">Address</span></p>
+    </div>
   )
 }
 
