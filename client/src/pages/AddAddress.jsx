35406eeece6a6c5b93c10e55aeba9229bc81933f
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-18 14:22:02 -0700

    add address image added

--- client/src/pages/AddAddress.jsx
+++ client/src/pages/AddAddress.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 // Input Field Component
 const inputField = (() => (
@@ -8,7 +9,11 @@ const inputField = (() => (
 const AddAddress = () => {
   return (
     <div className="mt-16 pb-16">
-        <p className="text-2xl md:text-3xl text-gray-500">Add Shipping <span className="font-semibold text-primary">Address</span></p>
+          <p className="text-2xl md:text-3xl text-gray-500">Add Shipping <span className="font-semibold text-primary">Address</span></p>
+          <div className="flex flex-col-reverse md:flex-row justify-between mt-10">
+              <div className=""></div>
+              <img src={assets.add_address_iamge} alt="Add Address" className="" />
+          </div>
     </div>
   )
 }
