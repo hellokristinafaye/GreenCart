f29cc72162c29c6bdec7ab00b776b0e8fa875fd2
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-18 17:11:28 -0700

    input placeholders set

--- client/src/components/seller/SellerLogin.jsx
+++ client/src/components/seller/SellerLogin.jsx
@@ -23,10 +23,15 @@ const SellerLogin = () => {
           
           <div className="flex flex-col gap-5 m-auto items-start p-8 py-12 min-w-80 sm:min-w-88 rounded-lg shadow-xl border border-gray-200">
               <p className="text-2xl font-medium m-auto"><span className="text-primary">Seller</span> Login</p>
-
+{/* email input */}
               <div className="w-full">
                   <p className="">Email</p>
-                  <input type="email" className="" />
+                  <input type="email" className="" placeholder="Enter your email"/>
+              </div>
+{/* password input */}
+              <div className="w-full">
+                  <p className="">Password</p>
+                  <input type="password" className="" placeholder="Enter your password"/>
               </div>
 
           </div>
