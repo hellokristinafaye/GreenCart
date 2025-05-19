4414bef2b5d0059d9c2f905b2383dbce2889d17e
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-18 17:09:57 -0700

    email input field added

--- client/src/components/seller/SellerLogin.jsx
+++ client/src/components/seller/SellerLogin.jsx
@@ -23,6 +23,12 @@ const SellerLogin = () => {
           
           <div className="flex flex-col gap-5 m-auto items-start p-8 py-12 min-w-80 sm:min-w-88 rounded-lg shadow-xl border border-gray-200">
               <p className="text-2xl font-medium m-auto"><span className="text-primary">Seller</span> Login</p>
+
+              <div className="w-full">
+                  <p className="">Email</p>
+                  <input type="email" className="" />
+              </div>
+
           </div>
           
     </form>
