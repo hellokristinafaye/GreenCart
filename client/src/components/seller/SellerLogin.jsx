9d36de0f3006be0f87a28b0a5d0898b47722d570
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-18 17:14:17 -0700

    button styled

--- client/src/components/seller/SellerLogin.jsx
+++ client/src/components/seller/SellerLogin.jsx
@@ -34,7 +34,7 @@ const SellerLogin = () => {
                   <input type="password" className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary" placeholder="Enter your password" required/>
               </div>
 
-              <button className="">Log In</button>
+              <button className="bg-primary text-white w-full py-2 rounded-md cursor-pointer">Log In</button>
 
           </div>
           
