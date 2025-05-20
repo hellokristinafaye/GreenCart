bb77f023a6b2e2ec07adb47e52d351e1430ded0b
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-19 21:04:44 -0700

    bug solved - this variable has to be inside a function in order to work with the function app. According to the red underline

--- client/src/pages/seller/ProductList.jsx
+++ client/src/pages/seller/ProductList.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { useAppContext } from '../../context/AppContext'
 
-const { products, currency } = useAppContext();
 
 const ProductList = () => {
-  return (
+        
+    const { products, currency } = useAppContext();
+    
+    return (
     
       <div className="no-scrollbar flex-1 h-[95vh] overflow-y-scroll flex flex-col justify-between">
             <div className="w-full md:p-10 p-4">
