ed1fb9279b28b4dcaa13043d3e5620380d4123e5
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-19 19:02:13 -0700

    all seller pages mounted

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,8 @@ import MyOrders from './pages/MyOrders'
 import SellerLogin from './components/seller/SellerLogin'
 import SellerLayout from './pages/seller/SellerLayout'
 import AddProduct from './pages/seller/AddProduct'
+import ProductList from './pages/seller/ProductList'
+import Orders from './pages/seller/Orders'
 
 const App = () => {
   
@@ -42,6 +44,8 @@ const App = () => {
 
           <Route path='/seller' element={isSeller ? <SellerLayout/> : <SellerLogin />}>
               <Route index element={isSeller ? <AddProduct/> : null } />
+              <Route path='/product-list' element={<ProductList/>} />
+              <Route path='/orders' element={<Orders/>} />
           </Route>
 
         </Routes>
