6f16e46350eff29a26a47dd31d22a2d1abd3971e
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-19 19:00:27 -0700

    addproduct tag started

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import AddAddress from './pages/AddAddress'
 import MyOrders from './pages/MyOrders'
 import SellerLogin from './components/seller/SellerLogin'
 import SellerLayout from './pages/seller/SellerLayout'
+import AddProduct from './pages/seller/AddProduct'
 
 const App = () => {
   
@@ -40,7 +41,7 @@ const App = () => {
           <Route path='/my-orders' element={<MyOrders />} />
 
           <Route path='/seller' element={isSeller ? <SellerLayout/> : <SellerLogin />}>
-
+              <Route index element={isSeller ? <AddProduct/> : null } />
           </Route>
 
         </Routes>
