5f7e5618e871c546e9fa3a0ac8184323357e9f6d
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-19 18:36:44 -0700

    logout function added and created

--- client/src/pages/seller/SellerLayout.jsx
+++ client/src/pages/seller/SellerLayout.jsx
@@ -14,15 +14,21 @@ const SellerLayout = () => {
         { name: "Orders", path: "/seller/orders", icon: assets.order_icon },
     ];
 
+    const logout = async () => {
+        setIsSeller(false);
+    }
+
     return (
         <>
             <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-300 py-3 bg-white">
+
                 <Link to='/'>
                     <img src={assets.logo} alt="logo" className="cursor-pointer w-34 md:w-38" />
                 </Link>
+
                 <div className="flex items-center gap-5 text-gray-500">
                     <p>Hi! Admin</p>
-                    <button className='border rounded-full text-sm px-4 py-1'>Logout</button>
+                    <button onClick={logout} className='border rounded-full text-sm px-4 py-1'>Logout</button>
                 </div>
             </div>
             <div className="md:w-64 w-16 border-r h-[550px] text-base border-gray-300 pt-4 flex flex-col transition-all duration-300">
