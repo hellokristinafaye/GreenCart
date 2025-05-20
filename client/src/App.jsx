10915136d6fd5f82fa53b601f61ba5c5d00243cd
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-19 18:22:24 -0700

    SellerLayout started

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -37,9 +37,11 @@ const App = () => {
           <Route path='/cart' element={<Cart/>} />
           <Route path='/add-address' element={<AddAddress/>} />
           <Route path='/my-orders' element={<MyOrders />} />
+
           <Route path='/seller' element={isSeller ? null : <SellerLogin />}>
 
           </Route>
+          
         </Routes>
       </div>
       {!isSellerPath && <Footer />}
