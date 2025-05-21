1a9b3f0058d925399a6569e1940ca069b25ebfaa
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-21 14:48:44 -0700

    cookie parser imported, but I didn't get an intellisense suggestion? And the .use was gonna be replaced with like, action?? I dunno, intellisense is not getting me

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 // tries a port in the .env file first, otherwise uses port 4000
@@ -6,6 +7,8 @@ const port = process.env.PORT || 4000;
 
 // Middleware config so all the data comes in as a json
 app.use(express.json());
+app.use(cookieParser());
+
 // when we send a request, the working response is below
 app.get('/', (req, res) => res.send("API is Working"));
 // start app, look for this console log for positive feedback
