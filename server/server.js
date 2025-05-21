1c383a0a2b33309a805245edf5a723b865074868
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-21 14:30:42 -0700

    OK I think the server is up??

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 // tries a port in the .env file first, otherwise uses port 4000
 const port = process.env.PORT || 4000;
 // when we send a request, the working response is below
-app.get('/', (req, res) => response.send("API is Working"));
+app.get('/', (req, res) => res.send("API is Working"));
 // start app, look for this console log for positive feedback
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
