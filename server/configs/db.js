25c51e1f9942485fdbedc59b1813f2a698c64d2f
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-21 16:07:33 -0700

    error msg set

--- server/configs/db.js
+++ server/configs/db.js
@@ -5,6 +5,6 @@ const connectDB = async () => {
         mongoose.connection.on('connected', () => console.log("Database Connected"));
         await mongoose.connect(`${process.env.MONGODB_URI}/greencart`)
     } catch (error) {
-        
+        console.error(error.message);
     }
 }
