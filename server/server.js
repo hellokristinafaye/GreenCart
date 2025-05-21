59b6f74bdfa350dbb0671595accb2583d831a29d
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-21 14:47:11 -0700

    first line of middleware

--- server/server.js
+++ server/server.js
@@ -3,9 +3,13 @@ import express from 'express';
 const app = express();
 // tries a port in the .env file first, otherwise uses port 4000
 const port = process.env.PORT || 4000;
+
+// Middleware config so all the data comes in as a json
+app.use(express.json());
 // when we send a request, the working response is below
 app.get('/', (req, res) => res.send("API is Working"));
 // start app, look for this console log for positive feedback
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
 })
+
