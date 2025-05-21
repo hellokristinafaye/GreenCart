23904492a13f28cd2b9562436e3a339358c384f6
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-21 16:04:45 -0700

    console log to denote whether the database is connected

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,3 +1,9 @@
 import mongoose from "mongoose";
 
-const connectDB = 
+const connectDB = async () => {
+    try {
+        mongoose.connection.on('connected', () => console.log("Database Connected"));
+    } catch (error) {
+        
+    }
+}
