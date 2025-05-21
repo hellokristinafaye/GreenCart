03b7e785299409b31898758bbd644792fde62654
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-21 16:58:25 -0700

    token and secret work

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/user.js";
 import bcrypt from "bcryptjs";
+import JWT from "jsonwebtoken";
 
 
 // Register User : /api/user/register
@@ -19,7 +20,8 @@ export const register = async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, 10);
         // creates new user with a hashed password
         const user = await User.create({name, email, password: hashedPassword})
-
+        // when the user is created, this creates a unique id for them
+        const token = JWT.sign({id: user._id}, )
     } catch (error) {
         
     }
