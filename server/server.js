2a7dfc5c38c3b698d1a082782bea1d2f97b95539
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-21 14:13:55 -0700

    req/res line

--- server/server.js
+++ server/server.js
@@ -4,3 +4,5 @@ import express from 'express';
 const app = express();
 // tries a port in the .env file first, otherwise uses port 4000
 const port = process.env.PORT || 4000;
+// when we send a request, the working response is below
+app.get('/', (req, res) => response.send("API is Working"));
