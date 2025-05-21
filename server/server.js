14d513d2bec58cffa60eb550c62e4f47811c7b7d
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-21 14:32:55 -0700

    Yeah that's the result. I hope that's the correct result lol

--- server/server.js
+++ server/server.js
@@ -7,6 +7,5 @@ const port = process.env.PORT || 4000;
 app.get('/', (req, res) => res.send("API is Working"));
 // start app, look for this console log for positive feedback
 app.listen(port, () => {
-    // console.log(`Server is running on http://localhost:${port}`)
-    console.log("hi!")
+    console.log(`Server is running on http://localhost:${port}`)
 })
