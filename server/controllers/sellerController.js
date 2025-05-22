da11e449fb8d5b7ed0717adebf82e44ec10b62bd
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-22 12:34:24 -0700

    Logout function copied to seller

--- server/controllers/sellerController.js
+++ server/controllers/sellerController.js
@@ -35,3 +35,19 @@ export const isSellerAuth = async (req, res) => {
         res.json({ success: false, message: error.message });
     }
 }
+
+
+// Logout Seller : /api/seller/logout
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie('token', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        });
+        return res.json({ success: true, message: "Logged Out" })
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
+    }
+}
