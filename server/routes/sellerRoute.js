2eeaf075ad958590c7ece4ab5a3a58195cbbebf2
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-22 12:41:43 -0700

    logout and .js added

--- server/routes/sellerRoute.js
+++ server/routes/sellerRoute.js
@@ -1,9 +1,11 @@
 
 
 import express from 'express';
-import { isSellerAuth, sellerLogin } from '../controllers/sellerController.js';
+import { isSellerAuth, sellerLogin, sellerLogout } from '../controllers/sellerController.js';
+import authSeller from '../middlewares/authSeller.js';
 
 const sellerRouter = express.Router();
 
 sellerRouter.post('/login', sellerLogin);
-sellerRouter.get('/is-auth', isSellerAuth);
+sellerRouter.get('/is-auth', authSeller, isSellerAuth);
+sellerRouter.get('/logout', sellerLogout);
