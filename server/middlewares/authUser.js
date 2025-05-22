112cbb74d670d0654dc81693cc926f6c8ebcbc3f
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-22 13:25:28 -0700

    OK I think that's it.  Honestly this one bug has taken ~4hrs to solve and it's just an empty object, kinda arbotrary issue... which I think is just a reality of coding unfortunately

--- server/middlewares/authUser.js
+++ server/middlewares/authUser.js
@@ -10,7 +10,6 @@ const authUser = async (req, res, next) => {
     try {
         // verifies the token w/ the secret
         const tokenDecode = jwt.verify(token, process.env.JWT_SECRET)
-        console.log(`tokenDecode.id: ${tokenDecode.id}`);
         // OK!! This tip was found on Stack Overflow and I *THINK* it's the right move?? either setting req.body to an empty object OR submitting an empty object as a req on Postman.  What a weird magical solve?? It doesn't exactly match the tutorial but it does produce the correct result.. and feels scalable. (5/22/25 1:20pm) Gonna remove all my messy work now. 
         req.body = {}
 
