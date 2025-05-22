e8cb73a76c3e09e44e70eef3453b62a4de4ac4fd
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-22 12:13:15 -0700

    seller auth middleware set

--- server/middlewares/authSeller.js
+++ server/middlewares/authSeller.js
@@ -8,14 +8,12 @@ const authSeller = async (req, res, next) => {
     }
     // I think this is the problem w/ authUser, b/c it SHOULD use userID w/o having to submit the specific user?? 
     try {
-        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET)
-        if (tokenDecode.id) {
-            req.body.userId = tokenDecode.id;
+        const tokenDecode = jwt.verify(sellerToken, process.env.JWT_SECRET)
+        if (tokenDecode.email === process.env.SELLER_EMAIL) {
+            next();
         } else {
             return res.json({ success: false, message: 'Not Authorized' });
         }
-        next();
-
     } catch (error) {
         res.json({ success: false, message: error.message });
     }
