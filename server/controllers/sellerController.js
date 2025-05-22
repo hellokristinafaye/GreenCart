abe597d92d2024c8e7bb3e2d0e3f674514c8a3fe
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-22 12:29:55 -0700

    try catch added plus error handling

--- server/controllers/sellerController.js
+++ server/controllers/sellerController.js
@@ -2,21 +2,26 @@ import jwt from 'jsonwebtoken';
 
 // Login Seller : /api/seller/login
 export const sellerLogin = async (req, res) => {
-    const { email, password } = req.body;
+   try {
+       const { email, password } = req.body;
 
-    if (password === process.env.SELLER_PASSWORD && email === SELLER_EMAIL) {
-        // creates token if email and password match
-        const token = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: '7d' });
+       if (password === process.env.SELLER_PASSWORD && email === SELLER_EMAIL) {
+           // creates token if email and password match
+           const token = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: '7d' });
 
-        res.cookie('sellerToken', token, {
-            httpOnly: true, //Prevents JS from accessing cookie
-            secure: process.env.NODE_ENV === 'production', // use secure cookies in production
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', // CSRF protection
-            maxAge: 7 * 24 * 60 * 60 * 1000, //Cookie expiration time (7days in miliseconds)
-        });
+           res.cookie('sellerToken', token, {
+               httpOnly: true, //Prevents JS from accessing cookie
+               secure: process.env.NODE_ENV === 'production', // use secure cookies in production
+               sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', // CSRF protection
+               maxAge: 7 * 24 * 60 * 60 * 1000, //Cookie expiration time (7days in miliseconds)
+           });
 
-        return res.json({ success: true, message: "Logged In - Seller" });
-    } else {
-        return res.json({ success: false, message: "Invalid Credentials - Seller" });
-    }
+           return res.json({ success: true, message: "Logged In - Seller" });
+       } else {
+           return res.json({ success: false, message: "Invalid Credentials - Seller" });
+       }
+   } catch (error) {
+       console.log(error.message);
+       res.json({ success: false, message: error.message });
+   }
 }
