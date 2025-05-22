dd6268d303d27fdfb009c2b74faf40427072092a
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-21 21:33:32 -0700

    logout endpoint done

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -99,9 +99,10 @@ export const logout = async (req, res) => {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-            
-        })
+        });
+        return res.json({success:true, message: "Logged Out" })
     } catch (error) {
-        
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
     }
 }
