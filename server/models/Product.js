3940355ec8b392dd0a5986b27b06ae67a307981a
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-22 16:06:59 -0700

    schema copied from User.js

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,5 +1,13 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-    
-})
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true},
+    cartItems: { type: Object, default: {}},
+}, { minimize: false })
+
+// checks if there is already a user, if not use the model schema
+const User = mongoose.models.user || mongoose.model('user', userSchema);
+
+export default User
