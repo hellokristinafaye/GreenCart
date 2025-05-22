236258088977fa9cc9cb656da70fae3b68abcfe4
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-21 21:16:21 -0700

    middleware for authUser started. JWT imported

---
+++ server/middlewares/authUser.js
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken';
+
+const authUser = async (req, res, next) => {
+    const { token } = req.cookies;
+
+    if (!token) {
+        return res.json({ success: false, message: 'Not Authorized' });
+    }
+
+    try {
+        const tokenDecode = jw
+    } catch (error) {
+        
+    }
+}
