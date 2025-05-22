c801cee1c51b0294adbca3161e14ae7aafa600ff
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-22 12:36:14 -0700

    seller logout set

--- server/controllers/sellerController.js
+++ server/controllers/sellerController.js
@@ -38,14 +38,14 @@ export const isSellerAuth = async (req, res) => {
 
 
 // Logout Seller : /api/seller/logout
-export const logout = async (req, res) => {
+export const sellerLogout = async (req, res) => {
     try {
-        res.clearCookie('token', {
+        res.clearCookie('sellerToken', {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
         });
-        return res.json({ success: true, message: "Logged Out" })
+        return res.json({ success: true, message: "Logged Out - Seller" })
     } catch (error) {
         console.log(error.message);
         res.json({ success: false, message: error.message });
