3d318874f14810d17d3fe2887f47690416c1577e
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-21 21:37:55 -0700

    isAuth added to userRoute

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { login, register } from '../controllers/userController.js';
+import { isAuth, login, register } from '../controllers/userController.js';
 // this is a userRouter
 const userRouter = express.Router();
 
 userRouter.post('/register', register);
 userRouter.post('/login', login);
+userRouter.get('/is-auth', isAuth);
+
 
 export default userRouter;
