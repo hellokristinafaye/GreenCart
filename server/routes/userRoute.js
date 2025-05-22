0360255409328a71e39e33a9e2c93ecf300557a8
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-21 19:27:14 -0700

    register imported from userController.js

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,1 +1,6 @@
 import express from 'express';
+import { register } from '../controllers/userController';
+// this is a userRouter
+const userRouter = express.Router();
+
+userRouter.post('/register', register);
