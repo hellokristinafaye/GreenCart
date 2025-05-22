6b05b7848199bb4a1bfb9c4a182feef597bdaadb
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-22 16:09:21 -0700

    price updated

--- server/models/Product.js
+++ server/models/Product.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
     description: { type: Array, required: true},
-    password: { type: String, required: true},
+    price: { type: Number, required: true},
     cartItems: { type: Object, default: {}},
 }, { minimize: false })
 
