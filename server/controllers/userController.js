f198ea1d279b27b4fa1ce6edc30158cd615d38ab
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-21 21:32:05 -0700

    try block of lotou

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -91,3 +91,17 @@ export const isAuth = async (req, res) => {
         res.json({ success: false, message: error.message });
     }
 }
+
+// Logout User : /api/user/logout
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie('token', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            
+        })
+    } catch (error) {
+        
+    }
+}
