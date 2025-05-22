7b2a4f364c7929478e2ac92d8241f9764f8dc908
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-22 12:09:48 -0700

    try statement added

--- server/middlewares/authSeller.js
+++ server/middlewares/authSeller.js
@@ -6,4 +6,17 @@ const authSeller = async (req, res, next) => {
     if (!sellerToken) {
         return res.json({ success: false, message: 'Not Authorized' });
     }
+    // I think this is the problem w/ authUser, b/c it SHOULD use userID w/o having to submit the specific user?? 
+    try {
+        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET)
+        if (tokenDecode.id) {
+            req.body.userId = tokenDecode.id;
+        } else {
+            return res.json({ success: false, message: 'Not Authorized' });
+        }
+        next();
+
+    } catch (error) {
+        res.json({ success: false, message: error.message });
+    }
 }
