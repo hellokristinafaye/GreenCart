6b8b2db61e83b5f5053e6dc7a67add6583bceafe
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-24 11:55:06 -0700

    update cart set

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -5,8 +5,10 @@ import User from "../models/user";
 export const updateCart = async (req, res) => {
     try {
         const { userId, cartItems } = req.body;
-        await User
+        await User.findByIdAndUpdate(userId, { cartItems });
+        res.json({ success: true, message: "Cart Updated" });
     } catch (error) {
-        
+        console.log(error.message)
+        res.json({ success: false, message: error.message });
     }
 }
