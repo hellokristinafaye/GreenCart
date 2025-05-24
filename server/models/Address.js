bc974d6e2280ef471e5860eaf296ed6945f2202f
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-24 13:59:53 -0700

    address model done. doesn't phone have to be a number?

--- server/models/Address.js
+++ server/models/Address.js
@@ -1,9 +1,16 @@
 import mongoose from 'mongoose';
 
-const addressSchema = mongoose.Schema({
+const addressSchema = new mongoose.Schema({
     userId: {type: String, required: true},
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
+    email: {type: String, required: true},
+    street: {type: String, required: true},
+    city: {type: String, required: true},
+    state: {type: String, required: true},
+    zipcode: {type: Number, required: true},
+    country: {type: String, required: true},
+    phone: {type: String, required: true},
 })
 // if the address is available, it gets used, otherwise it gets created
 const Address = mongoose.models.address || mongoose.model('address', addressSchema);
