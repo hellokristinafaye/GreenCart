7d20a8c7bb2d69fc6218dbfd66150a06ffd59b47
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-24 15:14:35 -0700

    start of order

--- server/models/Order.js
+++ server/models/Order.js
@@ -1,1 +1,8 @@
 import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+
+})
+
+// if an order exists it is called, if not it is created w/ the name order and an orderSchema
+const Order = mongoose.models.order || mongoose.model('order', orderSchema);
