3fcb07181f09fabb43f8472a921e6a6d4f1db97c
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-24 14:29:17 -0700

    address route file done

--- server/routes/addressRoute.js
+++ server/routes/addressRoute.js
@@ -1,7 +1,10 @@
 import express from 'express';
-import authUser from '../middlewares/authUser';
-import { addAddress } from '../controllers/addressController';
+import authUser from '../middlewares/authUser.js';
+import { addAddress, getAddress } from '../controllers/addressController.js';
 
 const addressRouter = express.Router();
 
 addressRouter.post('/add', authUser, addAddress);
+addressRouter.post('/get', authUser, getAddress);
+
+export default addressRouter;
