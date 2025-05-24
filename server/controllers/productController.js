0d668b5878c1dc9d129034cc18e53ddb05637fae
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-24 11:32:36 -0700

    Get single product route added

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -42,7 +42,16 @@ export const productList = async (req, res) => {
 
 // Get Single Product : /api/product/id
 export const productById = async (req, res) => {
-    
+    try {
+        const { id } = req.body
+        const product = await Product.findById({ id });
+        res.json({ success: true, product });
+
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
+    }
 } 
 
 // Change Product inStork : /api/product/stock
