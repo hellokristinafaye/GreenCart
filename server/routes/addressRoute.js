a910b57534ea9dedb59b468b67ca936df0d2c4b2
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-24 14:28:04 -0700

    middleware and controller imported

--- server/routes/addressRoute.js
+++ server/routes/addressRoute.js
@@ -1,2 +1,7 @@
 import express from 'express';
+import authUser from '../middlewares/authUser';
+import { addAddress } from '../controllers/addressController';
 
+const addressRouter = express.Router();
+
+addressRouter.post('/add', authUser, addAddress);
