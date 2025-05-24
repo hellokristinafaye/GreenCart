727459f78c27f01a9757ae0383a9d472e1f81300
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-24 14:03:01 -0700

    Address model imported

--- server/controllers/addressController.js
+++ server/controllers/addressController.js
@@ -1,6 +1,11 @@
-
+import Address from "../models/Address.js"
 
 // Add Address: /api/address/add
 export const addAddress = async (req, res) => {
-    
+    try {
+        const { address, userId } = req.body;
+        
+    } catch (error) {
+        
+    }
 }
