0677645bdfd9b8f447daa0d3f5c987f3a63c1c47
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-24 15:15:28 -0700

    order schema exported

--- server/models/Order.js
+++ server/models/Order.js
@@ -6,3 +6,5 @@ const orderSchema = new mongoose.Schema({
 
 // if an order exists it is called, if not it is created w/ the name order and an orderSchema
 const Order = mongoose.models.order || mongoose.model('order', orderSchema);
+
+export default Order;
