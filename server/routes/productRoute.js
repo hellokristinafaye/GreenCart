4a285f54d348adbb63c6856736aff7812001a00e
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-24 11:44:10 -0700

    addProduct from productController imported

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { upload} from '../configs/multer.js'
 import authSeller from '../middlewares/authSeller.js';
+import { addProduct } from '../controllers/productController';
 
 const productRouter = express.Router();
 
-productRouter.post('/add', upload.array([images]), authSeller);
+productRouter.post('/add', upload.array([images]), authSeller, addProduct);
+
 
