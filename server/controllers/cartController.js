abae716708fa8ea53197517280de5589145351b6
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-24 11:52:35 -0700

    user model imported

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -1,3 +1,12 @@
+import User from "../models/user";
 
 
 // Update User Cart Data:   /api/cart/update
+export const updateCart = async (req, res) => {
+    try {
+        const { userId, cartItems } = req.body;
+        await User
+    } catch (error) {
+        
+    }
+}
