5d3b4a9d75e348bfed330f86812509a8c2ea8cf9
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-24 15:24:19 -0700

    condition if no address or no items

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -3,7 +3,11 @@
 // Place Order COD: /api/order/cod
 export const placeOrderCOD = async (req, res) => {
     try {
-        
+        const { userId, items, address } = req.body;
+        if (!address || items.length === 0) {
+            return res.json({success:false, message: "Invalid Data"})
+        }
+        // Calculate Amount Using Items
     } catch (error) {
         
     }
