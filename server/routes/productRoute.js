4b5a9d6b0ae1b4dd5557526112327e4bbd7fd4e0
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-24 11:45:20 -0700

    route for individual products

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { upload} from '../configs/multer.js'
 import authSeller from '../middlewares/authSeller.js';
-import { addProduct } from '../controllers/productController';
+import { addProduct, productById } from '../controllers/productController';
 
 const productRouter = express.Router();
 
 productRouter.post('/add', upload.array([images]), authSeller, addProduct);
 productRouter.get('/list', productList);
+productRouter.get('/list', productById);
 
 
