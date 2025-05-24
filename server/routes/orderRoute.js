a06f8923dd83abbbabbad426592b6403a356a687
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-24 16:09:02 -0700

    placeOrder COD imporetd

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import authUser from '../middlewares/authUser.js';
+import { placeOrderCOD } from '../controllers/orderController.js';
 
 const orderRouter = express.Router();
 
-orderRouter.post('/cod', authUser)
+orderRouter.post('/cod', authUser, placeOrderCOD);
