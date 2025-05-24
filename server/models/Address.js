a00f7bcbfbed26cfa37b231adbcdb2cbf15df593
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-24 13:57:46 -0700

    address schema started

--- server/models/Address.js
+++ server/models/Address.js
@@ -1,9 +1,11 @@
 import mongoose from 'mongoose';
 
 const addressSchema = mongoose.Schema({
-
+    userId: {type: String, required: true},
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
 })
 // if the address is available, it gets used, otherwise it gets created
 const Address = mongoose.models.address || mongoose.model('address', addressSchema);
 
-export default
+export default Address;
