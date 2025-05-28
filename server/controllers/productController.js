b4e03ae2d230262487a0d4893db785595d83af7c
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-27 21:06:17 -0700

    OK giving up on Cloudinary for now... the code does work on the backend w/o images

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,42 +1,51 @@
 import { v2 as cloudinary } from "cloudinary";
+// import connectCloudinary from "../configs/cloudinary.js";
 import Product from "../models/Product.js";
-import { upload } from '../configs/multer.js'
+// import { upload } from '../configs/multer.js';
 
 // do we need {upload} from multer here?? 
 
 // Add Product : /api/product/add
 export const addProduct = async (req, res) => {
-    console.log("spot a")
 
     try {
+        // ------CLOUDINARY CODE START ---------
     // gets product info in the form of a JSON from the request body
         // let productData = JSON.parse(req.body.productData);
-        let productData  = req.body;
-//        
-// // takes images in the form of files
-        const images = req.files;
+//         let {productData}  = req.body;
+// //        
+// // // takes images in the form of files
+//         const images = [req.files];
+//         console.log("hello");
+//         console.log(images)
 
 //         let imagesUrl = await Promise.all(
-//             images.map(async (image) => {
-// //                 // gets the URL of the newly uploaded image, saved as "result"
-//                 let result = await cloudinary.uploader.upload(item.path, { resource_type: 'image' })
-// //                 // the url is made secure by this method.
+//             images.map(async (file) => {
+
+// // //                 // gets the URL of the newly uploaded image, saved as "result"
+//                 let result = await cloudinary.uploader.upload(file, { resource_type: 'image' })
+
+// // //                 // the url is made secure by this method.
 //                 return result.secure_url
 //             })
 //         )
-//     // creates product data in the database
-                await Product.create({...productData, image: images})
-        // let newProduct = {};
-        // await Product.create({
-        //     ...newProduct,
-        //     name: req.body.name,
-        //     description: req.body.description,
-        //     price: req.body.price,
-        //     offerPrice: req.body.offerPrice,
-        //     image: req.body.image,
-        //     category: req.body.category
-        // })
+// //     // creates product data in the database
+//         await Product.create({ ...productData, image: imagesUrl })
+        // ------CLOUDINARY CODE END ---------
 
+        // Temp code w/o Cloudinary Start
+        let newProduct = {};
+        await Product.create({
+            ...newProduct,
+            name: req.body.name,
+            description: req.body.description,
+            price: req.body.price,
+            offerPrice: req.body.offerPrice,
+            image: req.body.image,
+            category: req.body.category
+        })
+        // Temp code w/o Cloudinary End
+        
         res.json({ success: true, message: "Product Added!" });
         // OK the above code WORKS to upload stuff. We made the model not require anything just yet, and it'll get the names from the stuff... BUT we don't have cloudinary in here anywhere... At least it'll upload tho! 
 
