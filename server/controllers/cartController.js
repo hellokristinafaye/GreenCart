b73ed009d8ad9911366d797c31b49b4e9b2cbc8a
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-30 12:50:25 -0700

    just don't touch the cart page for right now. Let's finish the backend to the best of our ability and HOST THIS PAGE

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -3,11 +3,22 @@ import User from "../models/user.js";
 // 7:30 in tutorial
 // Update User Cart Data:   /api/cart/update
 export const updateCart = async (req, res) => {
+    // this is our best guess at updating cart items, but now the cart page won't even load. So we're trying an earlier draft below, but I didn't want to lose this tiny progress.
+    // try {
+    //     const { userId, cartItems } = req.body
+    //     await User.findByIdAndUpdate(userId, {cartItems: {"12344": 3}})
+    //     res.json({ success: true, message: userId })
+        
+    // } catch (error) {
+    //     console.log(error.message)
+    //     res.json({ success: false, message: error.message });
+    // }
+
     try {
         const { userId, cartItems } = req.body
-        await User.findByIdAndUpdate(userId, {cartItems: {"12344": 3}})
+        await User.findByIdAndUpdate(userId, { cartItems})
         res.json({ success: true, message: userId })
-        
+
     } catch (error) {
         console.log(error.message)
         res.json({ success: false, message: error.message });
